d9b3d0dec7ebc60e24f0f85bb8f6f21def1859fe
Selen <selenyanar@gmail.com>
2021-10-29 15:06:01 +0300

    Initial Commit

---
+++ Affirm/AffirmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmApp.swift
+//  Affirm
+//
+//  Created by Selen Yanar on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AffirmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Affirm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Affirm
+//
+//  Created by Selen Yanar on 29.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
