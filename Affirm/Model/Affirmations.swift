50a53a7ab628c52ac4c4edf5dea251f467de7546
Selen <selenyanar@gmail.com>
2021-11-03 11:16:58 +0300

    Initialize Affirmations Model

---
+++ Affirm/Model/Affirmations.swift
@@ -0,0 +1,55 @@
+//
+//  Affirmations.swift
+//  Affirm
+//
+//  Created by Selen Yanar on 3.11.2021.
+//
+
+import Foundation
+
+struct Affirmations {
+    
+    var health = [
+        "I move my body with joy and gratitude.",
+        "I am open to trying new things to improve my health.",
+        "I love my body and all that it does for me."
+    ]
+    
+    var personalGrowth = [
+        "I am at peace with where I am.",
+        "I accept myself exactly as I am now.",
+        "I am constantly growing, evolving and becoming my best self.",
+        "I am proud of who I am becoming.",
+        "I let go of my past beliefs that no longer serve me.",
+        "Being myself is enough.",
+        "My life is a process, and I accept change.",
+        "I grow from every life struggle."
+        
+    ]
+    
+    var love = [
+        "I find love everywhere I go.",
+        "Love starts with me.",
+        "I naturally find love everywhere.",
+        "I am creating my own love.",
+        "I am learning to love myself more every day.",
+        "My own love is a priority.",
+        "My heart is open."
+        
+    ]
+    
+    var career = [
+        "I am attracting great opportunities.",
+        "I focus on the lessons when problems arise"
+        
+        
+    ]
+    
+    var relationships = [
+        "I am attracting real connection.",
+        "I am magnetic to those around me.",
+        "I am attracting the right person for me."
+        
+    ]
+    
+}
